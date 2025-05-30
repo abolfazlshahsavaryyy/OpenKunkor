c7fbf4088b911e0f2a6e46875b9d06eec2385b35
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-30 18:19:48 +0330

    feat: add Answer class but not complete

---
+++ OpenKunkor/Services/OpenKunkor.Domain/Entities/Answer.cs
@@ -0,0 +1,29 @@
+﻿using OpenKunkor.Domain.Abstraction;
+using OpenKunkor.Domain.Arguments;
+using OpenKunkor.Domain.ValueObject.Answer;
+
+namespace OpenKunkor.Domain.Entities
+{
+    public class Answer:Entity<Guid>
+    {
+        public ContentAnswer content { get; set; }
+        public AnswerTime AnswerTime { get; set; }
+        public ViewCount View { get; set; }
+        public static Answer Create(ContentAnswer content)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(content.Value);
+            CustomArgumentException.ThrowIfNotInRange(content.Value, 0, 255);
+            return new Answer
+            {
+                content = content,
+                AnswerTime = AnswerTime.Of(DateTime.UtcNow),
+                View = ViewCount.Of(0)
+            };
+        }
+        public void Update(ContentAnswer content)
+        {
+            this.content = content;
+        }
+
+    }
+}
