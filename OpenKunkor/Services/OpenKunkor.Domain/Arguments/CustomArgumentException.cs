0d3043e3efe476241d8d8a5f4a500443a725cb48
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-30 18:18:56 +0330

    feat: add custom argument exception throw exception for domain validation

---
+++ OpenKunkor/Services/OpenKunkor.Domain/Arguments/CustomArgumentException.cs
@@ -0,0 +1,17 @@
+﻿
+namespace OpenKunkor.Domain.Arguments
+{
+    public class CustomArgumentException
+    {
+        public static void ThrowIfNotInRange(string content,int beginSize,int terminalSize)
+        {
+            if(content.Length<beginSize || content.Length > terminalSize)
+            {
+                throw new Exception($"out of the range.\ncontent: {content} " +
+                    $"\nbeginSize :{beginSize}" +
+                    $"\ncontentSize: {content.Length}" +
+                    $"\nterminalSize:{terminalSize}");
+            }
+        }
+    }
+}
