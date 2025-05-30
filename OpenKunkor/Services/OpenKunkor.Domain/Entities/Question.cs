b33c9de5c5c4f2a2aaca86f47abb6cbe79307ad9
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-30 18:19:29 +0330

    fix: edit the question entity better configuration for ddd

--- OpenKunkor/Services/OpenKunkor.Domain/Entities/Question.cs
+++ OpenKunkor/Services/OpenKunkor.Domain/Entities/Question.cs
@@ -1,18 +1,41 @@
 ﻿using OpenKunkor.Domain.Abstraction;
 using OpenKunkor.Domain.ValueObject.Question;
-using System.Runtime.CompilerServices;
-using System.Text.Json.Nodes;
-
+using OpenKunkor.Domain.Arguments;
 namespace OpenKunkor.Domain.Entities
 {
-    public class Question:Entity<Guid>
+    public class Question:Entity<QuestionId>
     {
-        public TitleQuestion Title { get; set; }
-        public ContentQuestion Content { get; set; }
-        public AskTime AskTime { get; set; }
-        public ViewCount View { get; set; }
+        public TitleQuestion Title { get; private set; }
+        public ContentQuestion Content { get; private set; }
+        public AskTime AskTime { get; private set; }
+        public ViewCount View { get; private set; }
 
-        public static Question(string title,string content,DateTime asktime)
+        public static Question Create(TitleQuestion title,ContentQuestion content)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(title.Value);
+            ArgumentException.ThrowIfNullOrWhiteSpace(content.Value);
+            CustomArgumentException.ThrowIfNotInRange(content.Value, 0, 255);
+            CustomArgumentException.ThrowIfNotInRange(title.Value, 0, 50);
+            var newQuestion = new Question
+            {
+                Title = title,
+                Content =content,
+                AskTime = AskTime.Of(DateTime.UtcNow),
+                View = ViewCount.Of(0)
+            };
+            return newQuestion;
+        }
+        public void Update(TitleQuestion newTitle,ContentQuestion newContent)
+        {
+            
+            ArgumentException.ThrowIfNullOrWhiteSpace(newTitle.Value);
+            ArgumentException.ThrowIfNullOrWhiteSpace(newContent.Value);
+            this.Title = newTitle;
+            this.Content = newContent;
+            
+            
+
+        }
 
 
     }
