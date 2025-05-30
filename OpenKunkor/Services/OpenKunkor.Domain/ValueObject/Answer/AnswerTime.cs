fbdbe90c6c0a104c788d2aaa9436b64aa3074cc3
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-05-30 18:23:06 +0330

    feat(domain): add answer value object (with problem)

---
+++ OpenKunkor/Services/OpenKunkor.Domain/ValueObject/Answer/AnswerTime.cs
@@ -0,0 +1,26 @@
+﻿namespace OpenKunkor.Domain.ValueObject.Answer
+{
+    public class AnswerTime
+    {
+        public DateTime Value { get; set; }
+        public AnswerTime(DateTime value)
+        {
+            this.Value = value;
+        }
+        public static AnswerTime Of(DateTime value)
+        {
+            if (value > DateTime.UtcNow)
+                throw new ArgumentException("AskTime cannot be in the future.");
+
+            return new AnswerTime(value);
+        }
+
+        public bool IsRecent(TimeSpan within) => DateTime.UtcNow - Value < within;
+
+        public override bool Equals(object? obj) =>
+            obj is AnswerTime other && Value.Equals(other.Value);
+
+        public override int GetHashCode() => Value.GetHashCode();
+
+    }
+}
