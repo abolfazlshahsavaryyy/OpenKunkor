1cee77c7ec1d7fd7d24ecd5b8e462353018c9adf
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-06-02 23:37:51 +0330

    feat: add two value objec to cohor

---
+++ OpenKunkor/Services/OpenKunkor.Domain/ValueObject/Cohort/CohorName.cs
@@ -0,0 +1,15 @@
+﻿namespace OpenKunkor.Domain.ValueObject.Cohort
+{
+    public class CohorName
+    {
+        public string Value { get; set; }
+        public static CohorName Of(string name)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(name);
+            return new CohorName
+            {
+                Value = name
+            };
+        }
+    }
+}
