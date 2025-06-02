0f3130c060b8f39c4f7f8a339496aba38eb8bf00
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-06-02 23:37:32 +0330

    feat: complete the cohor aggregate

--- OpenKunkor/Services/OpenKunkor.Domain/Entities/Cohort.cs
+++ OpenKunkor/Services/OpenKunkor.Domain/Entities/Cohort.cs
@@ -3,10 +3,22 @@ using OpenKunkor.Domain.ValueObject.Cohort;
 
 namespace OpenKunkor.Domain.Entities
 {
-    public class Cohort : Entity<CohortId>
+    public class Cohort : AggregateRoot<CohortId>
     {
-        public string Name { get; private set; }
+        public CohorName Name { get; private set; }
         public int Member { get; private set; }
+        public List<PhoneCoach> PhoneCoaches { get; set; } = new List<PhoneCoach>();
+        public static Cohort Create(CohorName name)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(name.Value);
+            return new Cohort
+            {
+                Name = name,
+                Member = 0
+            };
+            
+
+        }
 
 
     }
