d9222333bc6e832a77651733a228be39adb1f320
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-06-02 23:38:10 +0330

    feat:add userId implementation

--- OpenKunkor/Services/OpenKunkor.Domain/ValueObject/User/Common/UserId.cs
+++ OpenKunkor/Services/OpenKunkor.Domain/ValueObject/User/Common/UserId.cs
@@ -1,8 +1,17 @@
-﻿namespace OpenKunkor.Domain.ValueObject.User.Common
+﻿using OpenKunkor.Domain.Arguments;
+
+namespace OpenKunkor.Domain.ValueObject.User.Common
 {
     class UserId
     {
         public Guid Value { get; set; }
-
+        public static UserId Of(Guid Id)
+        {
+            CustomArgumentException.ThrowIfIdIsNull(Id);
+            return new UserId
+            {
+                Value = Id
+            };
+        }
     }
 }
