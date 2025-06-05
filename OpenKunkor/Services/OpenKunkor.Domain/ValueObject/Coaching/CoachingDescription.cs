85b1cf148d01e1e28138f8dc5dd6be6a55560e28
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-06-05 13:26:23 +0330

    feat: add Coaching Entity with its ValueObjects

---
+++ OpenKunkor/Services/OpenKunkor.Domain/ValueObject/Coaching/CoachingDescription.cs
@@ -0,0 +1,19 @@
+﻿namespace OpenKunkor.Domain.ValueObject.Coaching
+{
+    public sealed class CoachingDescription 
+    {
+        public string Value { get; }
+
+        private CoachingDescription(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Description cannot be empty.");
+
+            Value = value.Trim();
+        }
+
+        public static CoachingDescription Create(string value) => new CoachingDescription(value);
+
+        
+    }
+}
---
+++ OpenKunkor/Services/OpenKunkor.Domain/ValueObject/Coaching/CoachingTime.cs
@@ -0,0 +1,23 @@
+﻿namespace OpenKunkor.Domain.ValueObject.Coaching
+{
+    public sealed class CoachingTime 
+    {
+        public DateTime Start { get; }
+        public DateTime End { get; }
+        public TimeSpan TimeSpan { get; }
+
+        private CoachingTime(DateTime start, DateTime end)
+        {
+            if (end <= start)
+                throw new ArgumentException("End time must be after start time.");
+
+            Start = start;
+            End = end;
+            TimeSpan = end - start;
+        }
+
+        public static CoachingTime Create(DateTime start, DateTime end) => new CoachingTime(start, end);
+
+        
+    }
+}
