c86cdd6a8f81c471e9eba04be7e75663d1fe5ff2
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-06-10 23:07:29 +0330

    feat : add ApplicationUser for infrastructure layer

---
+++ OpenKunkor/Services/OpenKunkor.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿
+namespace OpenKunkor.Infrastructure.Identity
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Bio { get; set; } = null!;
+        public string ProfileUrl { get; set; } = null!;
+    }
+}
