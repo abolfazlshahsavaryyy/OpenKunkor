d878ebbb5b96dcf04dc7f4a576c4a5d25fbec2cc
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-06-10 23:07:56 +0330

    configure the AnswerConfiguration for Answer entity for ef core

---
+++ OpenKunkor/Services/OpenKunkor.Infrastructure/Data/Configuration/AnswerConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OpenKunkor.Domain.Entities;
+
+namespace OpenKunkor.Infrastructure.Data.Configuration
+{
+    class AnswerConfiguration : IEntityTypeConfiguration<Answer>
+    {
+        public void Configure(EntityTypeBuilder<Answer> builder)
+        {
+            builder.HasKey(a => a.Id);
+            builder.OwnsOne(a => a.Content, content =>
+            {
+                content.Property(c => c.Value)
+                .HasColumnName("Content")
+                .HasMaxLength(255)
+                .IsRequired();
+            });
+            builder.Property(a => a.Username)
+               .IsRequired()
+               .HasMaxLength(100);
+
+            builder.Property(a => a.Role)
+                   .IsRequired()
+                   .HasMaxLength(50);
+
+            builder.HasOne(a => a.Question)
+               .WithMany(q => q.Answers)
+               .HasForeignKey(a => a.QuestionId)
+               .OnDelete(DeleteBehavior.Cascade); // or Restrict if needed
+
+            builder.HasOne(a => a.User)
+                   .WithMany(u => u.Answers)
+                   .HasForeignKey(a => a.UserId)
+                   .OnDelete(DeleteBehavior.Cascade); // or Cascade
+        }
+    }
+}
