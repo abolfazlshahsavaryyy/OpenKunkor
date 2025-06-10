c1d3f961405eee60de2fe4d47ad0b2b8485b20b5
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-06-10 23:19:12 +0330

    feat: add relation for ApplicationUser Identity entity

--- OpenKunkor/Services/OpenKunkor.Infrastructure/Identity/ApplicationUser.cs
+++ OpenKunkor/Services/OpenKunkor.Infrastructure/Identity/ApplicationUser.cs
@@ -1,4 +1,7 @@
 ﻿
+using OpenKunkor.Domain.Entities;
+using OpenKunkor.Domain.Join;
+
 namespace OpenKunkor.Infrastructure.Identity
 {
     public class ApplicationUser:IdentityUser
@@ -7,5 +10,14 @@ namespace OpenKunkor.Infrastructure.Identity
         public string LastName { get; set; } = null!;
         public string Bio { get; set; } = null!;
         public string ProfileUrl { get; set; } = null!;
+        public ICollection<Content> Contents { get; set; } = new List<Content>();
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+        public ICollection<Question> Questions { get; set; } = new List<Question>();
+        public ICollection<Answer> Answers { get; set; } = new List<Answer>();
+        public ICollection<Message> Messages { get; set; } = new List<Message>();
+        public ICollection<UserContentLike> LikedContents { get; set; } = new List<UserContentLike>();
+        public ICollection<UserChat> Chats { get; set; } = new List<UserChat>();
+        public ICollection<UserFollow> Following { get; set; } = new List<UserFollow>();
+        public ICollection<UserFollow> Followers { get; set; } = new List<UserFollow>();
     }
 }
