9ecc4e0438e6489f65e74b5822602065cb8ae135
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-06-15 09:06:05 +0330

    feat: add UserChat relation configuration in ef core

---
+++ OpenKunkor/Services/OpenKunkor.Infrastructure/Data/Configuration/UserChatConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OpenKunkor.Domain.Join;
+
+namespace OpenKunkor.Infrastructure.Data.Configuration
+{
+    class UserChatConfiguration : IEntityTypeConfiguration<UserChat>
+    {
+        public void Configure(EntityTypeBuilder<UserChat> builder)
+        {
+            
+                builder.HasKey(x => new { x.UserId, x.ChatId });
+
+                // Map to ApplicationUser from Infrastructure
+                builder.HasOne<ApplicationUser>() // No nav property in domain
+                  .WithMany(a=>a.Chats)
+                  .HasForeignKey(uc => uc.UserId)
+                  .OnDelete(DeleteBehavior.Restrict);
+
+                builder.HasOne(uc => uc.Chat)
+                  .WithMany(c => c.Users)
+                  .HasForeignKey(uc => uc.ChatId)
+                  .OnDelete(DeleteBehavior.Restrict);
+            
+
+        }
+    }
+}
