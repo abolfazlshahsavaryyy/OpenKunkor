81bbb62fe5919767ea8c95d8a3b696bba8ad7da3
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-06-15 09:03:20 +0330

    fix: change relation for user in Join table UserChat

--- OpenKunkor/Services/OpenKunkor.Domain/Join/UserChat.cs
+++ OpenKunkor/Services/OpenKunkor.Domain/Join/UserChat.cs
@@ -2,7 +2,7 @@
 public class UserChat
 {
     public int UserId { get; set; }
-    public User User { get; set; } = null!;
+    public IUser User { get; set; } = null!;
 
     public int ChatId { get; set; }
     public Chat Chat { get; set; } = null!;
