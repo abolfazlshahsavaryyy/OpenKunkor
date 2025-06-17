dd6b50766e5b4c360eb763ecc87da7a5780e0cab
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-06-17 12:07:28 +0330

    fix: edit typo

--- OpenKunkor/Services/OpenKunkor.Domain/ValueObject/Question/QuestionTitle.cs
+++ OpenKunkor/Services/OpenKunkor.Domain/ValueObject/Question/QuestionTitle.cs
@@ -4,14 +4,14 @@ namespace OpenKunkor.Domain.ValueObject.Question
 {
     public class QuestionTitle
     {
-        public string Valeu { get; set; }
+        public string Value { get; set; }
         public static QuestionTitle Of(string value)
         {
             ArgumentException.ThrowIfNullOrEmpty(value);
             CustomArgumentException.ThrowIfNotInRange(value, 5, 100);
             return new QuestionTitle
             {
-                Valeu = value
+                    Value = value
             };
         }
     }
