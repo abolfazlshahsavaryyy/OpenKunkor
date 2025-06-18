4ca76f995d14dc9c40fe1b51253133a5d6fc7024
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-06-18 11:33:36 +0330

    feat: configure the many to many relation in ef core

---
+++ OpenKunkor/Services/OpenKunkor.Infrastructure/Data/Configuration/ManyToManyConfiguration/FollowingConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OpenKunkor.Domain.Join;
+
+namespace OpenKunkor.Infrastructure.Data.Configuration.ManyToManyConfiguration
+{
+    public class FollowingConfiguration : IEntityTypeConfiguration<UserFollow>
+    {
+        public void Configure(EntityTypeBuilder<UserFollow> builder)
+        {
+            builder.HasKey(f => new { f.FollowingId, f.FollowerId });
+
+            builder.HasOne<ApplicationUser>()
+                .WithMany(u => u.Followers)
+                .HasForeignKey(f => f.FollowerId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne<ApplicationUser>()
+                .WithMany(u => u.Following)
+                .HasForeignKey(f => f.FollowingId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ OpenKunkor/Services/OpenKunkor.Infrastructure/Data/Configuration/ManyToManyConfiguration/UserContentLikeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OpenKunkor.Domain.Join;
+
+namespace OpenKunkor.Infrastructure.Data.Configuration.ManyToManyConfiguration
+{
+    public class UserContentLikeConfiguration : IEntityTypeConfiguration<UserContentLike>
+    {
+        public void Configure(EntityTypeBuilder<UserContentLike> builder)
+        {
+            builder.HasKey(ucl => new { ucl.UserId, ucl.ContentId });
+
+            builder.HasOne<ApplicationUser>()
+                .WithMany(u => u.LikedContents)
+                .HasForeignKey(lc => lc.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(ucl => ucl.Content)
+                .WithMany(c => c.LikedByUsers)
+                .HasForeignKey(ucl => ucl.ContentId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
